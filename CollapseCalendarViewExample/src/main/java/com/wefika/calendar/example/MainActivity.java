8937c39d6d762186992bf3ef501bc02b00b1a1dd
Blaz Solar <solarblaz@gmail.com>
2014-07-10 13:34:13 +0200

    Adding readme file

--- CollapseCalendarViewExample/src/main/java/com/wefika/calendar/example/MainActivity.java
+++ CollapseCalendarViewExample/src/main/java/com/wefika/calendar/example/MainActivity.java
@@ -3,21 +3,16 @@ package com.wefika.calendar.example;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.wefika.calendar.CollapseCalendarView;
-
-import org.joda.time.LocalDate;
-
-
 public class MainActivity extends Activity {
 
-    private CollapseCalendarView mCalendarView;
+//    private CollapseCalendarView mCalendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mCalendarView = (CollapseCalendarView) findViewById(R.id.calendar);
-        mCalendarView.init(LocalDate.now(), LocalDate.now(), LocalDate.now().plusYears(1));
+//        mCalendarView = (CollapseCalendarView) findViewById(R.id.calendar);
+//        mCalendarView.init(LocalDate.now(), LocalDate.now(), LocalDate.now().plusYears(1));
     }
 }
---
+++ README.md
@@ -0,0 +1,19 @@
+CollapseCalendarView
+====================
+
+CollapseCalendarView is opensource Android Library that allows developers to easily add calendar to
+their app. CollapseCalendarView can be toggled between month and week view. 
+
+Usage
+-----
+```xml
+<com.wefika.calendar.CollapseCalendarView
+        android:id="@+id/calendar"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content" />
+```
+
+Example
+-------
+![Framed example screenshot](https://raw2.github.com/blazsolar/FlowLayout/master/images/framed_example_screenshot.png)
+Source code with examples is included in repository.
