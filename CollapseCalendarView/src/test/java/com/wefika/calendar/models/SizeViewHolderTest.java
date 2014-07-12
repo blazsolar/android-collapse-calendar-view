2af5f2d919c4d4271bc86706e77dda2ba1695c8e
Blaz Solar <solarblaz@gmail.com>
2014-07-12 19:09:39 +0200

    Model test

--- CollapseCalendarView/src/test/java/com/wefika/calendar/models/SizeViewHolderTest.java
+++ CollapseCalendarView/src/test/java/com/wefika/calendar/models/SizeViewHolderTest.java
@@ -2,10 +2,12 @@
 package com.wefika.calendar.models;
 
 import android.view.View;
+import android.view.ViewGroup;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.Mockito;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
@@ -47,6 +49,117 @@ public class SizeViewHolderTest {
         assertEquals(300, mViewHolder.getHeight());
         assertEquals(25, mViewHolder.getMinHeight());
         assertEquals(325, mViewHolder.getMaxHeight());
+
+    }
+
+    @Test
+    public void testOnFinishDone() throws Exception {
+
+        View view = mViewHolder.getView();
+        ViewGroup.LayoutParams params = mock(ViewGroup.LayoutParams.class);
+        when(view.getLayoutParams()).thenReturn(params);
+
+        mViewHolder.onFinish(true);
+
+        verify(view).setVisibility(View.VISIBLE);
+        assertEquals(ViewGroup.LayoutParams.WRAP_CONTENT, params.height);
+
+    }
+
+    @Test
+    public void testOnFinishNotDone() throws Exception {
+
+        View view = mViewHolder.getView();
+        ViewGroup.LayoutParams params = mock(ViewGroup.LayoutParams.class);
+        when(view.getLayoutParams()).thenReturn(params);
+
+        mViewHolder.onFinish(false);
+
+        verify(view).setVisibility(View.GONE);
+        assertEquals(ViewGroup.LayoutParams.WRAP_CONTENT, params.height);
+
+    }
+
+    @Test
+    public void testAnimateStart() throws Exception {
+
+        View view = mViewHolder.getView();
+        ViewGroup.LayoutParams params = mock(ViewGroup.LayoutParams.class);
+        when(view.getLayoutParams()).thenReturn(params);
+
+        mViewHolder.animate(11);
+
+        verify(view).setVisibility(View.VISIBLE);
+        assertEquals(1f / 100 * 100, params.height, 0);
+        verify(view).requestLayout();
+
+    }
+
+    @Test
+    public void testAnimateFinishExpand() throws Exception {
+
+        View view = mViewHolder.getView();
+        ViewGroup.LayoutParams params = mock(ViewGroup.LayoutParams.class);
+        when(view.getLayoutParams()).thenReturn(params);
+
+        mViewHolder.animate(11);
+
+        reset(view);
+        reset(params);
+        when(view.getLayoutParams()).thenReturn(params);
+
+        mViewHolder.animate(112);
+
+        verify(view).setVisibility(View.VISIBLE);
+        assertEquals(ViewGroup.LayoutParams.WRAP_CONTENT, params.height);
+
+    }
+
+    @Test
+    public void testAnimateFinishCollapse() throws Exception {
+
+        View view = mViewHolder.getView();
+        ViewGroup.LayoutParams params = mock(ViewGroup.LayoutParams.class);
+        when(view.getLayoutParams()).thenReturn(params);
+
+        mViewHolder.animate(11);
+
+        reset(view);
+        reset(params);
+        when(view.getLayoutParams()).thenReturn(params);
+
+        mViewHolder.animate(0);
+
+        verify(view).setVisibility(View.GONE);
+        assertEquals(ViewGroup.LayoutParams.WRAP_CONTENT, params.height);
+
+    }
+
+    @Test
+    public void testAnimateDirectFinishExpand() throws Exception {
+
+        View view = mViewHolder.getView();
+        ViewGroup.LayoutParams params = mock(ViewGroup.LayoutParams.class);
+        when(view.getLayoutParams()).thenReturn(params);
+
+        mViewHolder.animate(112);
+
+        verify(view).setVisibility(View.VISIBLE);
+        assertEquals(ViewGroup.LayoutParams.WRAP_CONTENT, params.height);
+
+    }
+
+    @Test
+    public void testAnimateDirectFinishedCollapse() throws Exception {
+
+        View view = mViewHolder.getView();
+        ViewGroup.LayoutParams params = mock(ViewGroup.LayoutParams.class);
+        when(view.getLayoutParams()).thenReturn(params);
+
+        mViewHolder.animate(0);
+
+        verify(view).setVisibility(View.GONE);
+        assertEquals(ViewGroup.LayoutParams.WRAP_CONTENT, params.height);
 
     }
 }
